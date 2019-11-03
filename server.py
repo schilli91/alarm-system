4cb6e8fe29f03fc9c3495394269cb717037e1697
Moritz Schillinger <MoritzSchillinger@web.de>
2019-11-03 22:04:53 +0100

    Added status panel to server index page.

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request, redirect, url_for
-import os.path
 import configparser
-from config.config import CONFIG_DIR
 import logging
+import os.path
+from datetime import datetime, timedelta
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from config.config import CONFIG_DIR
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -14,13 +17,33 @@ if not os.path.isfile(CONFIG_DIR):
 
 config = configparser.ConfigParser()
 
+POLL_STATUS_GREEN = 'status-green'
+POLL_STATUS_RED = 'status-red'
+
 
 @app.route('/', methods=['GET'])
 def index():
+    poll_status = POLL_STATUS_RED
+    try:
+        with open('last_poll.txt', 'r') as poll_file:
+            last_poll = poll_file.read()
+        last_poll_date = datetime.strptime(last_poll, '%d.%m.%Y %H:%M:%S')
+        poll_threshold_red = datetime.now() - timedelta(hours=6)
+
+        if poll_threshold_red < last_poll_date:
+            poll_status = POLL_STATUS_GREEN
+
+    except OSError:
+        last_poll = 'Keine Daten vorhanden.'
+
     config.read(CONFIG_DIR)
-    info = {'to_number': config["DEFAULT"]["TO_NUMBER"],
-            'sleep_after_call': config["DEFAULT"]["SLEEP_AFTER_CALL"],
-            'from_number': config["TWILIO"]["FROM_NUMBER"], }
+    info = {
+        'to_number': config["DEFAULT"]["TO_NUMBER"],
+        'sleep_after_call': config["DEFAULT"]["SLEEP_AFTER_CALL"],
+        'from_number': config["TWILIO"]["FROM_NUMBER"],
+        'last_poll': last_poll,
+        'poll_status': poll_status,
+    }
 
     return render_template('index.html', **info)
 
@@ -54,4 +77,4 @@ def set_configs():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
